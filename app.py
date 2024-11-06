eb82390807dd09b22af7b832983c74adbc942fa2
Ashim Sijapati <177884190+Ashimsjpt@users.noreply.github.com>
2024-11-06 11:37:10 -0500

    set up bacis HTML, and upload function. Also added basic file for style and output.

--- app.py
+++ app.py
@@ -4,4 +4,31 @@ import numpy as np
 import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
+#upload
+def index():
+    return render_template('index.html') 
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files.get('file')
+    z_height = request.form.get('z_height', '5')  #defult size of z height 5mm
+
+    if not file or file.filename == '':
+        return "No file selected", 400
+    if not z_height.isdigit():
+        return "Invalid z-height", 400
+    
+# save file
+ file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+ file.save(file_path)
+
+
+#edge detection outline 
+
+
+
+# convert to stl
